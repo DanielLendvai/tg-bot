755891ec3033812ac86a555c6cd76947518fa339
Daniel <daniel.lendvai@bitninja.io>
2024-10-15 11:59:16 +0200

    .

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const bot = new TelegramBot(token);
 const app = express();
 
 
-bot.setWebHook(`https://tg-b5vmt8fuh-daniellendvais-projects.vercel.app`);
+bot.setWebHook(`https://tg-b5vmt8fuh-daniellendvais-projects.vercel.app/api`);
 
 app.use(express.json());
 
