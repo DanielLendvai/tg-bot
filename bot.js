9ffc06a72c5a65ddc123180a1b026212d686ba84
Daniel <daniel.lendvai@bitninja.io>
2024-10-19 14:12:00 +0200

    .

--- bot.js
+++ bot.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const PORT = process.env.PORT || 3000;
-const URL = `https://tg-bot-git-main-daniellendvais-projects.vercel.app/api`;
-
-const bot = new TelegramBot(token, {
-  webHook: {
-    port: PORT,
-  },
-});
-
-bot.setWebHook(`${URL}`);
-
 const app = express();
 
-app.use(bot.webHookCallback('/api'));
+app.use(express.json());
 
-const members = ['Márk', 'Máté', 'Norbi', 'Dani'];
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token);
+
+const url = 'https://tg-bot-git-main-daniellendvais-projects.vercel.app/api'; 
+const webhookPath = `/bot${token}`;
+bot.setWebHook(url + webhookPath);
+
+app.post(webhookPath, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
@@ -27,6 +26,4 @@ bot.on('message', (msg) => {
     bot.sendMessage(chatId, 'Who is Tibi?');
   }
 });
-
-
 module.exports = app;
