73ef75acf2df13c894e7c6cc9bd4716ee2306b02
Daniel <daniel.lendvai@bitninja.io>
2024-10-19 13:53:44 +0200

    .

--- bot.js
+++ bot.js
@@ -1,42 +1,32 @@
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '8146313524:AAG2XTS6iDVNqZj80MnOb4Kq64zCNuLgd-k';
-const bot = new TelegramBot(token);
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const PORT = process.env.PORT || 3000;
+const URL = `https://tg-bot-git-main-daniellendvais-projects.vercel.app/api`;
+
+const bot = new TelegramBot(token, {
+  webHook: {
+    port: PORT,
+  },
+});
+
+bot.setWebHook(`${URL}/api`);
+
 const app = express();
 
-
-bot.setWebHook(`https://tg-bot-git-main-daniellendvais-projects.vercel.app/api`);
-
-app.use(express.json());
-
-app.post(`/api`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
+app.use(bot.webHookCallback('/api'));
 
 const members = ['Márk', 'Máté', 'Norbi', 'Dani'];
 
-bot.onText(/\/kiabuzi/, (msg) => {
-  const chatId = msg.chat.id;
-
-  const randomIndex = Math.floor(Math.random() * members.length);
-  const selectedMember = members[randomIndex];
-
-  bot.sendMessage(chatId, `🎉 Te vagy a buzi: ${selectedMember}! 🎉`);
-})
-
-// Listen for any message that contains "Tibi"
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
 
-  if (messageText && messageText.toLowerCase().includes("tibi")) {
-    bot.sendMessage(chatId, "De ki az a Tibi?");
+  if (messageText && messageText.toLowerCase().includes('tibi')) {
+    bot.sendMessage(chatId, 'Who is Tibi?');
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+module.exports = app;
